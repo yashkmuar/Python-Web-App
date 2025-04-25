d725dcafb81326feee79e2118a6cd20b16efb4ae
adityatomar <adityatomar@microsoft.com>
2025-04-25 14:34:16 +0530

    revert api name

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/query', methods=['POST'])
+@app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
 
